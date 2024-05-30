0b814ef8fcae91bac58ba942c3fec09901b11c9d
홍일권 <hik10417@gmail.com>
2024-05-30 12:06:38 +0900

    sigleton complete

---
+++ src/creational/singleton/Singleton.kt
@@ -0,0 +1,30 @@
+package creational.singleton
+
+
+/*
+   Singleton pattern
+*/
+
+
+
+class Singleton private constructor(val data: String) {
+
+    companion object {
+        @Volatile
+        private var instance: Singleton? = null
+
+        fun getInstance(data: String): Singleton {
+            var result = instance
+            if (result == null) {
+                synchronized(this) {
+                    if (result == null) {
+                        result = Singleton(data)
+                        instance = result
+                    }
+                }
+            }
+            return result!!
+        }
+    }
+}
+
